f85a9ad76a064d20a8409ba3ec5d41692e60e42d
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-20 19:18:51 +0800

    Update yjw0420-class-Student.kt

--- app/src/main/java/com/example/dan/yjw0420-class-Student.kt
+++ app/src/main/java/com/example/dan/yjw0420-class-Student.kt
@@ -1,7 +1,8 @@
 package com.example.dan
 
-class `yjw0420-class-Student`: `yjw0420-class-Person`() {
-    var sno = ""
-    var grade = 0
+// 主构造函数
+class `yjw0420-class-Student`(val sno:String, val grade:Int): `yjw0420-class-Person`() {
+    val student = `yjw0420-class-Student`("a123",5)
+    
 }
 
