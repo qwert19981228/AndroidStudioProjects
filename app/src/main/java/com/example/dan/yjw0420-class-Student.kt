cf7743f9218aceeeb5e18eb2e5e106bfbff42dbd
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-20 21:11:13 +0800

    Update yjw0420-class-Student.kt

--- app/src/main/java/com/example/dan/yjw0420-class-Student.kt
+++ app/src/main/java/com/example/dan/yjw0420-class-Student.kt
@@ -1,8 +1,23 @@
 package com.example.dan
 
 // 主构造函数
-class `yjw0420-class-Student`(val sno:String, val grade:Int): `yjw0420-class-Person`() {
-    val student = `yjw0420-class-Student`("a123",5)
+//class `yjw0420-class-Student`(val sno:String, val grade:Int, name:String, age:Int): `yjw0420-class-Person`(name,age) {
+//    init{
+//        println("sno is " + sno)
+//        println("grade is " + grade)
+//    }
+// 次构造函数
+//    constructor(name: String,age: Int) : this("",0,name,age){
+//
+//    }
+//    constructor():this("",0){
+//
+//    }
+//}
 
+// 继承
+class `yjw0420-class-Student`(name:String, age:Int): `yjw0420-class-Person`(name,age),`yjw0420-class-Study`{
+    override fun readBooks() {
+        println(name + " is reading")
+    }
 }
-
