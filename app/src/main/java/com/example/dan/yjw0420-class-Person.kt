c16e9f23e0c64974e815353838ad09a191d30b9c
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-20 21:11:03 +0800

    Update yjw0420-class-Person.kt

--- app/src/main/java/com/example/dan/yjw0420-class-Person.kt
+++ app/src/main/java/com/example/dan/yjw0420-class-Person.kt
@@ -1,17 +1,9 @@
 package com.example.dan
 
-open class `yjw0420-class-Person` {
-    var name = ""
-    var age = 0
+open class `yjw0420-class-Person`(val name:String,val age:Int) {
 
     fun eat(){
         println(name + " is eating. He is " + age + " year old.")
     }
 }
 
-fun main(){
-    val p = `yjw0420-class-Person`()
-    p.name = "Jack"
-    p.age = 19
-    p.eat()
-}
