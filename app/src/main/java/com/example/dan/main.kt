837a61774a094b4aa1875b35494854fd780bf044
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-21 09:23:00 +0800

    Update main.kt

--- app/src/main/java/com/example/dan/main.kt
+++ app/src/main/java/com/example/dan/main.kt
@@ -3,6 +3,13 @@ package com.example.dan
 fun main(){
     val student = `yjw0420-class-Student`("Jack",19)
     doStudy(student)
+
+    val cellphone1 = `yjw0421-class-Cellphone`("Samsung",1299.99)
+    val cellphone2 = `yjw0421-class-Cellphone`("Samsung",1299.99)
+    println(cellphone1)
+    println("cellphone1 equals cellphone2 " + (cellphone1 == cellphone2))
+
+    `yjw0421-object-Singleton`.singletonTest()
 }
 
 fun doStudy(study: `yjw0420-class-Study`){
