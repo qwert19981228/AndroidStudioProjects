fe39c02e9ed6d8324e181de3143bbb624b2b347f
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-23 13:02:26 +0800

    Update yjw0423-小魔术.kt

--- app/src/main/java/com/example/dan/yjw0423-小魔术.kt
+++ app/src/main/java/com/example/dan/yjw0423-小魔术.kt
@@ -1,2 +1,16 @@
 package com.example.dan
 
+
+fun main(){
+    // $ 字符串内嵌表达式
+    val brand = "Samsung"
+    val price = 1299.99
+    println("Cellphone(brand=$brand,price=$price)")
+    printParams(str = "world")
+}
+
+// 给函数设定参数默认值
+fun printParams(num:Int = 100,str:String){
+    println("num is $num, str is $str")
+
+}
