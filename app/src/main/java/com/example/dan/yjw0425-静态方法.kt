96d74ea9f56967de00c234dd9fa49f40770c755a
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-25 17:12:57 +0800

    Create yjw0425-静态方法.kt

---
+++ app/src/main/java/com/example/dan/yjw0425-静态方法.kt
@@ -0,0 +1,18 @@
+package com.example.dan
+
+//object Util{
+//    fun doAction(){
+//        println("do Action")
+//    }
+//}
+
+class Util{
+   fun doAction1(){
+       println("do Action1")
+   }
+    companion object{
+        fun doAction2(){
+            println("do Action2")
+        }
+    }
+}
