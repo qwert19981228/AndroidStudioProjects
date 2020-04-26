8d21456ddce4a241f198c6f6f4933f207f91b5bf
qwert19981228 <34954566+qwert19981228@users.noreply.github.com>
2020-04-26 08:42:09 +0800

    Update yjw0425-静态方法.kt

--- app/src/main/java/com/example/dan/yjw0425-静态方法.kt
+++ app/src/main/java/com/example/dan/yjw0425-静态方法.kt
@@ -10,7 +10,9 @@ class Util{
    fun doAction1(){
        println("do Action1")
    }
+
     companion object{
+        @JvmStatic
         fun doAction2(){
             println("do Action2")
         }
